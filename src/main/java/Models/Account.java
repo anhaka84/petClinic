c7eb79de5cfa98ccf6f549fb799520111efb3286
tuananhvu1234 <tuananhcho1234@gmail.com>
2022-11-14 17:22:55 +0700

    xoa comment
    xoa comment

--- src/main/java/Models/Account.java
+++ src/main/java/Models/Account.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Models;
 
-/**
- *
- * @author Hoang Anh
- */
 public class Account {
 //    test pull and push
 //    tuan anh
