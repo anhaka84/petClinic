dd58e884dfa454f438e23e3022e4e3be313b7b6f
Doan Hoang Anh <98464268+anhakadoan84@users.noreply.github.com>
2022-11-27 21:52:12 +0700

    Fix admin page

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.aptech.mavenproject2.petclinic {
     requires javafx.base;
     requires java.desktop;
 //    requires pdfbox;
+    requires java.base;
 
     opens com.aptech.mavenproject2.petclinic to javafx.fxml;
     exports com.aptech.mavenproject2.petclinic;
