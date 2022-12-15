3d5014c9e003419d7526df21321de1d4c9302545
nguyenthuminh <100056751+nguyenthuminh@users.noreply.github.com>
2022-12-15 23:29:38 +0700

    create controller
    Tạo controller cho cus va doc

---
+++ src/main/java/Controller/Doctor/DoctorBookingController.java
@@ -0,0 +1,23 @@
+package Controller.Doctor;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class DoctorBookingController implements Initializable{
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
